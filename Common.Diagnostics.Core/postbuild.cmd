0e6dfc63dfa11302bd5f21b8a3fcaedb11d10149
Dario Airoldi <darioa@microsoft.com>
2020-05-31 23:26:23 +0200

    postbuild fix

--- Common.Diagnostics.Core/postbuild.cmd
+++ Common.Diagnostics.Core/postbuild.cmd
@@ -1,13 +1,15 @@
 echo on
 set env=DEV
-IF NOT DEFINED UpdateSetup set UpdateSetup=false
+IF NOT DEFINED UpdateSetup set UpdateSetup=true
 
 rem set TargetBin=C:\temp\New folder
 set TargetBin=E:\dev\01. ABB Port\Ekip Connect 3\EkipConnect\bin\Debug
 set env=DEV
 
-IF /I %UpdateSetup%==True echo  robocopy .\bin\Debug\net472 "%TargetBin%" *.dll *.pdb
-IF /I %UpdateSetup%==True robocopy .\bin\Debug\net472 "%TargetBin%" Common.Diagnostics*.dll
-IF /I %UpdateSetup%==True robocopy .\bin\Debug\net472 "%TargetBin%" Common.Diagnostics*.pdb
+IF %OutDir% == bin\Debug\net472\ (
+	IF /I %UpdateSetup%==True echo  robocopy .\%OutDir% "%TargetBin%" *.dll *.pdb
+	IF /I %UpdateSetup%==True robocopy .\%OutDir% "%TargetBin%" Common.Diagnostics*.dll
+	IF /I %UpdateSetup%==True robocopy .\%OutDir% "%TargetBin%" Common.Diagnostics*.pdb
+)
 
  
