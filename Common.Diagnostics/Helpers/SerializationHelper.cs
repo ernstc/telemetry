203c389a20ad0a74d87e653688491bad69a16c54
Dario Airoldi <darioa@microsoft.com>
2020-06-01 12:01:21 +0200

    packages image and cleanup

--- Common.Diagnostics/Helpers/SerializationHelper.cs
+++ Common.Diagnostics/Helpers/SerializationHelper.cs
@@ -14,10 +14,6 @@ using System.Text.Json;
 
 namespace Common
 {
-    //public class UpperCaseNamingPolicy : JsonNamingPolicy
-    //{
-    //    public override string ConvertName(string name) => name.ToUpper();
-    //}
     internal class SerializationHelper
     {
         #region SerializeJson<T>(T obj)
