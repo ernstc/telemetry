08440c98954d3859ae0b13b3c7a6a2e660cf4b81
Dario Airoldi <darioa@microsoft.com>
2020-08-17 13:41:14 +0200

    samples fix

--- Samples/01. wpf/EasySample462/MainWindow.xaml.cs
+++ Samples/01. wpf/EasySample462/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,7 +14,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes; 
+using System.Windows.Shapes;
+using Metrics = System.Collections.Generic.Dictionary<string, object>; // $$$
 #endregion
 
 namespace EasySample462
@@ -28,5 +30,67 @@ namespace EasySample462
                 InitializeComponent();
             }
         }
+
+        private void MainWindow_Initialized(object sender, EventArgs e)
+        {
+            using (var sec = this.GetCodeSection(new { sender = sender.GetLogString(), e = e.GetLogString() }))
+            {
+                sec.Debug("this is a debug trace", "User", properties: new Dictionary<string, object>() { { "", "" } });
+                sec.Information("this is a Information trace", "Raw");
+                sec.Warning("this is a Warning trace", "User.Report");
+                sec.Error("this is a error trace", "Resource");
+            }
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            using (var sec = this.GetCodeSection(new { sender = sender.GetLogString(), e = e.GetLogString() }))
+            {
+                try
+                {
+                    sec.Debug("this is a debug trace", "User", new Metrics() {
+                        { "User", 123 },
+                        { "Tags", new[] { "sample", "user", "advanced" } }
+                    });
+
+                    sec.Information("this is a Information trace", "event");
+                    sec.Information("this is a Information trace", "Raw");
+                    sec.Warning("this is a Warning trace", "User.Report");
+                    sec.Error("this is a error trace", "Resource");
+
+
+                    throw new NullReferenceException();
+                }
+                catch (Exception ex)
+                {
+                    sec.Exception(ex);
+                }
+
+
+                // report button 
+                // var recorder = Trace.Listeners.OfType<TraceListener>().FirstOrDefault(l => l is RecorderTraceListener) as RecorderTraceListener;
+                // var entries = recorder.GetItems();
+            }
+        }
+
+        public void SampleMethod()
+        {
+            using (var sec = this.GetCodeSection())
+            {
+                Thread.Sleep(100);
+                SampleMethodNested();
+                SampleMethodNested1();
+
+            }
+        }
+        public void SampleMethodNested()
+        {
+            Thread.Sleep(100);
+        }
+        public void SampleMethodNested1()
+        {
+            Thread.Sleep(10);
+        }
+
     }
 }
