bd5f44c115f0873e3676eaf386d442ee63815bdd
Dario Airoldi <darioa@microsoft.com>
2021-04-01 15:55:53 +0200

    samples fix

--- Samplesv2/03. Aspnet Blazor/EasySampleBlazorv2/Server/Program.cs
+++ Samplesv2/03. Aspnet Blazor/EasySampleBlazorv2/Server/Program.cs
@@ -26,13 +26,13 @@ namespace EasySampleBlazorv2.Server
                               var log4NetProvider = new Log4NetProvider(options);
                               loggingBuilder.AddDiginsightFormatted(log4NetProvider, context.Configuration);
 
-                              TelemetryConfiguration telemetryConfiguration = new TelemetryConfiguration("6600ae1e-1466-4ad4-aea7-c017a8ab5dce");
-                              ApplicationInsightsLoggerOptions appinsightOptions = new ApplicationInsightsLoggerOptions();
-                              var tco = Options.Create<TelemetryConfiguration>(telemetryConfiguration);
-                              var aio = Options.Create<ApplicationInsightsLoggerOptions>(appinsightOptions);
-                              loggingBuilder.AddDiginsightJson(new ApplicationInsightsLoggerProvider(tco, aio), context.Configuration);
+                              //TelemetryConfiguration telemetryConfiguration = new TelemetryConfiguration("6600ae1e-1466-4ad4-aea7-c017a8ab5dce");
+                              //ApplicationInsightsLoggerOptions appinsightOptions = new ApplicationInsightsLoggerOptions();
+                              //var tco = Options.Create<TelemetryConfiguration>(telemetryConfiguration);
+                              //var aio = Options.Create<ApplicationInsightsLoggerOptions>(appinsightOptions);
+                              //loggingBuilder.AddDiginsightJson(new ApplicationInsightsLoggerProvider(tco, aio), context.Configuration);
 
-                              loggingBuilder.AddFilter<ApplicationInsightsLoggerProvider>("", LogLevel.Trace);
+                              //loggingBuilder.AddFilter<ApplicationInsightsLoggerProvider>("", LogLevel.Trace);
 
                           });
 
