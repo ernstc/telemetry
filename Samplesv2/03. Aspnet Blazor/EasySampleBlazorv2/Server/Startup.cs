366204287e52f9aa9d4aa1221f467f9a5ec9085e
Dario Airoldi <dario.airoldi@hotmail.com>
2021-04-01 17:30:24 +0200

    blazor app sample v2

--- Samplesv2/03. Aspnet Blazor/EasySampleBlazorv2/Server/Startup.cs
+++ Samplesv2/03. Aspnet Blazor/EasySampleBlazorv2/Server/Startup.cs
@@ -27,43 +27,39 @@ namespace EasySampleBlazorv2.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            using (var scope = _logger.BeginMethodScope())
-            {
-                services.AddControllersWithViews();
-                services.AddRazorPages();
-            }
+            using var scope = _logger.BeginMethodScope();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            using (var scope = _logger.BeginMethodScope())
+            using var scope = _logger.BeginMethodScope();
+            if (env.IsDevelopment())
             {
-                if (env.IsDevelopment())
-                {
-                    app.UseDeveloperExceptionPage();
-                    app.UseWebAssemblyDebugging();
-                }
-                else
-                {
-                    app.UseExceptionHandler("/Error");
-                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                    app.UseHsts();
-                }
-
-                app.UseHttpsRedirection();
-                app.UseBlazorFrameworkFiles();
-                app.UseStaticFiles();
-
-                app.UseRouting();
-
-                app.UseEndpoints(endpoints =>
-                {
-                    endpoints.MapRazorPages();
-                    endpoints.MapControllers();
-                    endpoints.MapFallbackToFile("index.html");
-                });
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
+            });
         }
     }
 }
